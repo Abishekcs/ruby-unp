6d29685ac2762c409479adedc9948e53b8716da7
Abishekcs <abishekdascs@gmail.com>
2026-01-03 22:21:00 +0530

    Document daytime TCP server code with inline comments
    Add detailed comments explaining:
    - Socket API wrapper functions (Socket, Bind, Listen, Accept, etc.)
    - IPv4 address configuration and network byte order conversions
    - Daytime protocol (RFC 867) implementation on port 13
    - Server's connect-send-close pattern for each client connection
    - Buffer usage and time formatting with ctime()
    
    No functional changes to the code.

--- C/day_time_tcp_srv.c
+++ C/day_time_tcp_srv.c
@@ -15,35 +15,68 @@
 #include "wrap_write.h"
 #include "wrap_close.h"
 //---------------------
-#define MAXLINE 4096
-#define LISTENQ 1024
-#define SA struct sockaddr 
+#define MAXLINE 4096    // Maximum line length for buffer
+#define LISTENQ 1024    // Maximum number of client connections to queue
+#define SA struct sockaddr  // Shorthand for struct sockaddr type casting
 
-
+/*
+ * Simple Daytime TCP Server
+ * 
+ * This server implements a basic daytime protocol (RFC 867) that:
+ * 1. Listens on port 13 (standard daytime port)
+ * 2. Accepts incoming client connections
+ * 3. Sends the current server time to each client
+ * 4. Closes the connection immediately after sending
+ * 
+ * The server uses wrapper functions (Socket, Bind, Listen, etc.) which
+ * likely include error handling around the standard socket API calls.
+ */
 int main(int argc, char **argv) {
-	int					listenfd, connfd;
-	struct sockaddr_in	servaddr;
-	char				buff[MAXLINE];
-	time_t				ticks;
+	int					listenfd, connfd;  // File descriptors: listenfd for listening socket, connfd for connected client
+	struct sockaddr_in	servaddr;          // Server address structure to hold IP and port information
+	char				buff[MAXLINE];     // Buffer to store the message sent to clients
+	time_t				ticks;             // Variable to store current time in seconds since epoch
 
+	// Create a TCP socket (AF_INET = IPv4, SOCK_STREAM = TCP)
 	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
+	// Zero out the server address structure to ensure clean initialization
 	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family      = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(13);	/* daytime server */
+	
+	// Configure server address structure
+	servaddr.sin_family      = AF_INET;           // Use IPv4 address family
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections on any network interface (0.0.0.0)
+	servaddr.sin_port        = htons(13);	      // Bind to port 13 (daytime server standard port)
+	                                               // htons() converts port to network byte order (big-endian)
 
+	// Bind the socket to the specified address and port
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
-  Listen(listenfd, LISTENQ);
+	// Mark the socket as a passive socket that will accept incoming connections
+	// LISTENQ specifies the maximum length of the queue of pending connections
+	Listen(listenfd, LISTENQ);
 
+	// Infinite loop to continuously accept and service client connections
 	for ( ; ; ) {
-	  connfd = Accept(listenfd, (SA *) NULL, NULL);
+		// Accept a new client connection (blocks until a client connects)
+		// NULL parameters mean we don't care about the client's address information
+		connfd = Accept(listenfd, (SA *) NULL, NULL);
 
-    ticks = time(NULL);
-    snprintf(buff, sizeof(buff), "Hello From Server %.24s\r\n", ctime(&ticks));
-    Write(connfd, buff, strlen(buff));
+		// Get current system time in seconds since Unix epoch (Jan 1, 1970)
+		ticks = time(NULL);
+		
+		// Format the message with current time
+		// ctime() converts time_t to human-readable string format (e.g., "Fri Jan  3 21:14:22 2026\n")
+		// %.24s limits the string to 24 characters (excludes the newline that ctime adds)
+		snprintf(buff, sizeof(buff), "Hello From Server %.24s\r\n", ctime(&ticks));
+		
+		// Send the formatted message to the connected client
+		Write(connfd, buff, strlen(buff));
 
-	  Close(connfd);
+		// Close the connection with this client
+		// The server follows a connect-send-close pattern for each client
+		Close(connfd);
 	}
+	// Note: This infinite loop means the program never reaches here
+	// In production, you'd want signal handlers for graceful shutdown
 }
